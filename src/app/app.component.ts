e240a28a01a8ceae21b9deb115b5cc9813fc7981
Eliran Eliassy <eliran.eliassy@gmail.com>
2018-11-27 12:03:24 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,19 @@
+
+
+
 import { Component } from '@angular/core';
+import { Item } from './item';
+
+
+
+import { db } from './db';
+
+export class Item2 {
+  title: string;
+  constructor(title: string) {
+    this.title = title;
+  }
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +21,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'cellebrite-course-project';
+
+  items: Item[] = [];
+
+  shoppingCart: Item[] = [];
+
+  constructor() {
+    this.items = db;
+  }
+
+  addToCart(item: Item): void {
+    this.shoppingCart.push(item);
+  }
+
+  existInCart(item: Item): boolean {
+    return this.shoppingCart.includes(item);
+  }
+
+
 }
+
+
---
+++ src/app/item.ts
@@ -0,0 +1,66 @@
+
+
+    export interface Category {
+        href: string;
+        name: string;
+    }
+
+    export interface Order {
+        href: string;
+    }
+
+    export interface OrderEmail {
+        href: string;
+    }
+
+    export interface MerchantData {
+        _id: string;
+        updateTime: number;
+        createTime: number;
+        hidden: boolean;
+        name: string;
+        logoUrl?: any;
+        serviceFormUrl: string;
+        servicePhoneNumber: string;
+        priceDropPolicyUrl: string;
+        returnPolicyUrl: string;
+        websiteUrl: string;
+        editable: boolean;
+        supportsPriceDrop: boolean;
+        href: string;
+        serviceEmail: string;
+    }
+
+    export interface Item {
+        _id: string;
+        updateTime: number;
+        href: string;
+        description: string;
+        productCode?: any;
+        purchaseDate: string;
+        price: number;
+        quantity: number;
+        productUrl?: any;
+        imageUrl: string;
+        imageWidth: number;
+        imageHeight: number;
+        returnByDate?: any;
+        category: Category;
+        parsingConfidence: number;
+        returend: boolean;
+        order: Order;
+        shipments: any[];
+        orderEmails: OrderEmail[];
+        shipmentEmails: any[];
+        priceHistory: any[];
+        merchantData: MerchantData;
+        userName: string;
+        userFullName: string;
+        userImageUrl: string;
+        fashbashId: string;
+        productVisibility: boolean;
+    }
+
+    
+
+
